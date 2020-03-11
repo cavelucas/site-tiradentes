623dcc952925458cf775698f6856d23228a97eb1
Goulart <macbook@Goulart-Mac-Mini.local>
2020-03-11 18:46:12 -0300

    v1.3 finalização do portal de aulas

--- single.php
+++ single.php
@@ -1,26 +1,21 @@
 <?php get_header(); ?>
 
-<!-- este era o archive.php do site -->
-
-    <div class="archive">
-    
-        <h2>Últimas aulas postadas</h2>
+    <div class="post">
     
         <?php 
             if ( have_posts() ) : 
                 while ( have_posts() ) : the_post(); ?>
 
-                <div class="archive__item">
+                <div class="post__item">
                     <h2>
                         <a href="<?php the_permalink();?>"><?php the_title();?></a>
                     </h2>
-                    <p class="professor-nome" >Professor: <?php the_author();?></p>
+                    <p class="professor-nome">Professor: <?php the_author();?></p>
                     <h3>
                         <?php the_category(); ?>
                     </h3>
                     <p>
-                        <?php the_excerpt();?>
-                        <a class="ver-mais" href="<?php the_permalink(); ?>">Ver completo</a>
+                        <?php the_content();?>
                     </p>
                 </div>
 
